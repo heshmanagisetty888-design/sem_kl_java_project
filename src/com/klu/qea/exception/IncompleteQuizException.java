84e11b5d0c3623c39c172233cb05fe0cc40deac4
heshmanagisetty888-design <heshmanagisetty888@gmail.com>
2025-12-16 10:38:35 +0530

    On branch main

---
+++ src/com/klu/qea/exception/IncompleteQuizException.java
@@ -0,0 +1,11 @@
+package com.klu.qea.exception;
+
+public class IncompleteQuizException extends RuntimeException {
+
+    public IncompleteQuizException(String message) {
+        super(message);
+    }
+}
+
+
+
